836fde569a06521ee2f9d1d3cb8cb41110a3cbc5
Matt White <16320656+matt-FFFFFF@users.noreply.github.com>
2024-11-11 16:51:15 +0000

    test: add unhappy path tests

--- armparser/tolower_test.go
+++ armparser/tolower_test.go
@@ -2,6 +2,7 @@ package armparser
 
 import (
 	"context"
+	"errors"
 	"testing"
 
 	"github.com/matt-FFFFFF/goarmfunctions/logger"
@@ -36,6 +37,24 @@ func TestToLower(t *testing.T) {
 			expected: "hello",
 			ctx:      nil,
 			err:      nil,
+		},
+		{
+			desc:     "wrong args",
+			in:       "[toLower()]",
+			expected: "",
+			ctx:      nil,
+			err: &ArgumentError{
+				"toLower",
+				1,
+				0,
+			},
+		},
+		{
+			desc:     "not string",
+			in:       "[toLower(1)]",
+			expected: "",
+			ctx:      nil,
+			err:      errors.New("toLower argument must be a string, got int"),
 		},
 	}
 	ctx := context.WithValue(context.Background(), logger.LoggerContextKey, logger.NewDebugLogger())
